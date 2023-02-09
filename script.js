54a82a186ad0045445d7dca55f5a1d1a588b5736
Fire-Fox <101003448+fiersclem@users.noreply.github.com>
2023-02-09 23:07:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -24,56 +24,135 @@ window.onload = () => {
     let themeLink = document.getElementById("theme-link")
     let image =document.getElementById("theme")
     let background=document.getElementById("background")
+    let map =document.getElementById("map")
    
     if(localStorage.theme != null){
         themeLink.href = `style_${localStorage.theme}.css`
         image.innerHTML=`<img src="./assets/${localStorage.theme}.svg " height="60px">`
-        background.innerHTML=`<img src="./assets/jour.png" width="100%" class="background">`
+        background.innerHTML=`<img src="./assets/${localStorage.background}.png" width="100%" class="background">`
+        map.innerHTML=`<img src="./assets/${localStorage.map}.png" height="60px">`
     }else{
         themeLink.href = "style_clair.css"
         image.innerHTML=`<img src="./assets/clair.svg " height="60px">`
         background.innerHTML=`<img src="./assets/jour.png" width="100%" class="background">`
+        map.innerHTML=`<img src="./assets/itinéraire.png" height="60px">`
         localStorage.theme = "clair"
+        localStorage.background="jour"
+        localStorage.map="itinéraire"
     }
 
     document.getElementById("theme").addEventListener("click", function(){
         if (localStorage.theme == "clair") {
             localStorage.theme = "sombre"
-            background.innerHTML=`<img src="./assets/nuit.png" width="100%" class="background">`
+            localStorage.background="nuit"
+            localStorage.map="itinérairen"
         } else {
             localStorage.theme = "clair"
-            background.innerHTML=`<img src="./assets/jour.png" width="100%" class="background">`
+            localStorage.background="jour"
+            localStorage.map="itinéraire"
         }
         themeLink.href = `style_${localStorage.theme}.css`
         image.innerHTML=`<img src="./assets/${localStorage.theme}.svg " height="60px">`
+        background.innerHTML=`<img src="./assets/${localStorage.background}.png" width="100%" class="background">`
+        map.innerHTML=`<img src="./assets/${localStorage.map}.png" height="60px">`
     })
 
 //--------------------------vue
-    let image2 =document.getElementById("test")
+    let switchh =document.getElementById("switch")
 
-    if(localStorage.rien != null){
-        image2.innerHTML=`<img src="./assets/${localStorage.rien}.png " height="40px">`
+    if(localStorage.switch != null){
+        switchh.innerHTML=`<img src="./assets/${localStorage.switch}.png " height="40px">`
     }else{
-        image2.innerHTML=`<img src="./assets/vert.png " height="50px">`
-        localStorage.rien = "vert"
+        switchh.innerHTML=`<img src="./assets/vert.png " height="50px">`
+        localStorage.switch = "vert"
     }
     
-    document.getElementById("test").addEventListener("click", function(){
-        if (localStorage.rien == "vert") {
-            localStorage.rien = "rouge"
+    document.getElementById("switch").addEventListener("click", function(){
+        if (localStorage.switch == "vert") {
+            localStorage.switch = "rouge"
             if(confirm==1){
                 writer();
             }
         } else {
-            localStorage.rien = "vert"
+            localStorage.switch = "vert"
             if(confirm==1){
                 writev();
             }
         }
-        image2.innerHTML=`<img src="./assets/${localStorage.rien}.png " height="40px">`
+        switchh.innerHTML=`<img src="./assets/${localStorage.switch}.png " height="40px">`
+    })
+
+    list=document.getElementById("list")
+    document.getElementById("map").addEventListener("click", function(){
+        if (localStorage.map == "itinéraire") {
+            localStorage.map = "itinérairen"
+            list.innerHTML=`<div class="map">
+            <input id="searchbard" type="text" name="search" placeholder="Search ICAO Departure">
+            <input id="searchbara" type="text" name="search" placeholder="Search ICAO Arrival">
+            <button style="position: relative; z-index: 2;"id="button">clique ici</button>
+            <p id="km"></p>
+        </div>
+        `
+        } else {
+            localStorage.map = "itinéraire"
+            list.innerHTML=``
+    
+        }
+        map.innerHTML=`<img src="./assets/${localStorage.map}.png" height="60px">`
+        document.getElementById("button").addEventListener("click", function(){
+            getDistance()
+        
+           })
     })
 
 }
+
+
+async function getAirportData(icao) {
+    // Récupère les informations de l'aéroport à partir de son code ICAO
+    const API_KEY = "9538afb46af4469ba60962c4e2";
+     url = `https://api.checkwx.com/metar/${icao}/decoded?&format=json&lang=fr&distance=250&limit=1&hours=1&key=9538afb46af4469ba60962c4e2`;
+     response = await fetch(url);
+     data = await response.json();
+    return {
+      lat: data.data[0].station.geometry.coordinates[0],
+      lon: data.data[0].station.geometry.coordinates[1],
+    };
+  }
+  
+  async function getDistance() {
+    // Demande les codes ICAO des deux aéroports à l'utilisateur
+    icao1 = document.getElementById('searchbard').value
+    icao1=icao1.toLowerCase();
+    icao2 = document.getElementById('searchbara').value
+    icao2=icao2.toLowerCase();
+  
+    // Récupère les informations des aéroports à partir de leur code ICAO
+     airport1 = await getAirportData(icao1);
+     airport2 = await getAirportData(icao2);
+  
+    // Calcule la distance en miles entre les deux aéroports
+     lat1 = airport1.lat;
+     lon1 = airport1.lon;
+     lat2 = airport2.lat;
+     lon2 = airport2.lon;
+     radius = 6371; // Rayon de la Terre en km
+     dLat = (lat2 - lat1) * (Math.PI / 180);
+     dLon = (lon2 - lon1) * (Math.PI / 180);
+     a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) *
+        Math.cos(lat2 * (Math.PI / 180)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+     c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+     distance = radius * c;
+  
+    // Affiche la distance entre les deux aéroports
+    document.getElementById("km").innerHTML=`La distance entre les aéroports ${icao1} et ${icao2} est de ${distance} km.`
+  }
+  
+
 
 function search_icao() {
   let input = document.getElementById('searchbar').value
@@ -118,11 +197,13 @@ function search_icao() {
             qnh_hg=response.barometer.hg
             lon=response.station.geometry.coordinates[0]
             lat=response.station.geometry.coordinates[1]
+            console.log(lat)
+            console.log(lon)
         })
     },
   }).mount('#list');
 
-  if(localStorage.rien=="vert"){
+  if(localStorage.switch=="vert"){
     confirm=1;
     writev()
 }
@@ -158,7 +239,7 @@ function writev(){
             </div>
         </div>
       `
-    let star=document.getElementById("star")
+    /*let star=document.getElementById("star")
 
     if(localStorage.fav != null){
         star.innerHTML=`<img class="star" src="./assets/${localStorage.fav}.png " height="30px">`
@@ -175,7 +256,7 @@ function writev(){
             localStorage.fav = "starv"
         }
         star.innerHTML=`<img class="star" src="./assets/${localStorage.fav}.png " height="30px">`
-    })
+    })*/
 }
 
 
@@ -213,7 +294,7 @@ function writer(){
         </div>
       `
 
-    let star=document.getElementById("star")
+    /*let star=document.getElementById("star")
 
     if(localStorage.fav != null){
         star.innerHTML=`<img class="star" src="./assets/${localStorage.fav}.png " height="50px">`
@@ -230,7 +311,7 @@ function writer(){
             localStorage.fav = "starv"
         }
         star.innerHTML=`<img class="star" src="./assets/${localStorage.fav}.png " height="50px">`
-    })
+    })*/
 }
 
 
